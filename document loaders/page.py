114c3188fdb892907bcde4d3841a3acb5219b0e0
sujaysuresh365 <sujaysuresh365@gmail.com>
2026-08-14 23:06:25 +0530

    Remove unused

--- document loaders/page.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import WebBaseLoader
-
-url = "https://www.apple.com/in/macbook-pro/"
-
-data = WebBaseLoader(url)
-
-docs = data.load()
-
-print(docs[0].page_content)
